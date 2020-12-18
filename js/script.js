4f0f8594766ffa4471c0d0c07b28a8de99df8126
whydoifly <60507914+whydoifly@users.noreply.github.com>
2020-12-18 14:39:43 +0300

    v.0.1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+var mySwiper = new Swiper ('.swiper-container', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+			clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
